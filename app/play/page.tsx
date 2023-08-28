2d7cbdc6a7f0f6ad15dc57f0b5a8061855779b78
Lautaro Ivan Gonzalez <glautaroivan@gmail.com>
2023-08-27 22:38:29 -0300

    delete unused page router

--- app/play/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Play() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
-      <div className="z-10 max-w-5xl w-full items-center justify-between lg:flex">
-        <h1 className="text-2xl mb-10 text-center">Sliding game play</h1>
-      </div>
-    </main>
-  );
-}
